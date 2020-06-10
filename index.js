51b4a5c56bf75b65f06d36b45571eda9f6891ee5
Luis Salazar <luis.salazar@sr-consultores.com>
2020-06-10 13:12:59 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const actores = [{
+  nombre: 'Jack',
+  apellido: 'Nicholson',
+  ocupacion: 'Actor'
+},
+{
+  nombre: 'Kate',
+  apellido: 'Winslet',
+  ocupacion: 'Actriz'
+}]
+
+for (const actor of actores) {
+  console.log(actor);
+}
